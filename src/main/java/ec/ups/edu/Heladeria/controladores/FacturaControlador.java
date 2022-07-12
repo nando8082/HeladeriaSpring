77138f3105b2e9d3cba916b520d09b678546f223
nando8082 <daniel.pinargo@gmail.com>
2022-07-11 20:02:32 -0500

    Arreglo Crear Factura

--- src/main/java/ec/ups/edu/Heladeria/controladores/FacturaControlador.java
+++ src/main/java/ec/ups/edu/Heladeria/controladores/FacturaControlador.java
@@ -3,6 +3,7 @@ package ec.ups.edu.Heladeria.controladores;
 import ec.ups.edu.Heladeria.entidades.Cliente;
 import ec.ups.edu.Heladeria.entidades.Factura;
 import ec.ups.edu.Heladeria.servicios.FacturaServicio;
+import ec.ups.edu.Heladeria.servicios.UsuarioServicio;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,26 +20,32 @@ import java.util.Optional;
 public class FacturaControlador {
 
     private FacturaServicio facturaServicio;
+    private UsuarioServicio usuarioServicio;
 
     @Autowired
     public void setFacturaServicio(FacturaServicio facturaServicio) {
         this.facturaServicio = facturaServicio;
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllFacturas(HttpSession httpSession) {
+    @Autowired
+    public void setUsuarioServicio(UsuarioServicio usuarioServicio) {
+        this.usuarioServicio = usuarioServicio;
+    }
 
-        String v = (String) httpSession.getAttribute("Verificador");
-
-        if(v== "true"){
-            long id = (long) httpSession.getAttribute("idCliente");
-
-            List<Factura> listaFacturas = facturaServicio.findAll();
-
-            return new ResponseEntity<List<Factura>>(listaFacturas,HttpStatus.OK);
+    @GetMapping("/cliente") //obtener el listado de Facturas
+    public ResponseEntity<List<Factura>> getAllFacturasCliente(HttpSession httpSession) {
+        long idCl = (long) httpSession.getAttribute("idCliente");
+        System.out.println(idCl);
+        Optional<Cliente> clienteOptional = Optional.ofNullable(usuarioServicio.retrieveUsuarioById(idCl));
+        if(clienteOptional.isEmpty()){
+            return ResponseEntity.badRequest().build();
         }
 
-        return  ResponseEntity.badRequest().body("NO HA INICIADO SESION");
+        long idC = clienteOptional.get().getId();
+        System.out.println(idC);
+        List<Factura> facturaList = facturaServicio.retrieveIdCliente(idC);
+        //System.out.println(facturaList);
+        return new ResponseEntity<List<Factura>>(facturaList, HttpStatus.OK);
     }
 
 
